686931b015af0c92e1a089a4530c79ea9421d35f
deCha <decha92@hotmail.com>
2021-12-27 14:56:19 -0600

    Tested edge cases and modified code restrict inputs

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -15,9 +15,50 @@ const Modal = (props) => {
 	const [newName, setNewName] = useState(currentKitty.name);
 	const [newURL, setNewURL] = useState(currentKitty.thumbnailURL);
 	const [newBirthDate, setNewBirthDate] = useState(currentKitty.birthDate);
-	const [newOwner, setNewOwner] = useState(currentKitty.ownerName);
+  const [newOwner, setNewOwner] = useState(currentKitty.ownerName);
+  
+  let today = new Date();
+  let currentDate = `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()};`
+  let minDate = "2000-01-01"
+
+  function checkURL(link) {
+    let isValid;
+    try {
+        new URL(link);
+      } catch (event) {
+        console.error(event);
+        isValid = false;
+        return isValid;
+      }
+      isValid = true;
+      return isValid; 
+    };
+
+    function checkDate(value) {
+      if (value > currentDate) {
+        alert("You must select a date that is not in the future.")
+        setNewBirthDate("");
+        return;
+      } else if (value < minDate){
+        alert("You must choose a valid date")
+        setNewBirthDate("");
+        return;
+      } else {
+        setNewBirthDate(value);
+      }
+    }
 
 	function updateKitty() {
+    let validatedLink = checkURL(newURL);
+
+    if(!validatedLink) {
+      alert("You must enter a valid URL");
+      setNewURL("");
+      return;
+    }
+
+    checkDate(newBirthDate);
+
     let newKittyList = kittyList.slice();
 
 		let updatedKitty = {
@@ -45,7 +86,28 @@ const Modal = (props) => {
 
 	function cancelChanges() {
 		setShowModal(false);
-	}
+  }
+
+  function stringContainsNumber(string) {
+    return /\d/.test(string);
+  }
+
+  function containsSpecialChars(string) {
+    let specialChars = /[!@#$%^&*()_+\-=[\]{};':"|,.<>/?]+/;
+    return specialChars.test(string);
+  }
+
+  function checkName(value) {
+    let checkString1 = stringContainsNumber(value);
+    let checkString2 = containsSpecialChars(value);
+    let trimmedName = value.trim();
+    
+    if (checkString1 || checkString2 || trimmedName === "") {
+      alert("You cannot use any characters but letters.")
+    } else {
+      setNewName(value);
+    }
+  }
 
 	return (
 		<>
@@ -68,14 +130,16 @@ const Modal = (props) => {
 							type="text"
 							id="modalName"
 							placeholder="Enter Name"
-							onChange={(event) => setNewName(event.target.value)}
+							onChange={(event) => checkName(event.target.value)}
 						></input>
 						<label>Birth date</label>
 						<input
 							type="date"
-							id="modalBirthDate"
-							placeholder="Enter Birth Date"
-							onChange={(event) => setNewBirthDate(event.target.value)}
+              placeholder="Enter Birth Date"
+              min="1995-01-01"
+              max={currentDate}
+              onChange={(event) => 
+                setNewBirthDate(event.target.value)}
 						></input>
 						<label className="owner-Select">Owner</label>
             <select 
